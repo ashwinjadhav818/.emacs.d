73b49b233803c8bc3e159e2d80a8b6dd103dbdb6
A S H⚡ <81229115+ashwinjadhav818@users.noreply.github.com>
2025-11-16 18:07:34 +0530

    feat: new org capture options

--- lisp/packages/org-config.el
+++ lisp/packages/org-config.el
@@ -29,15 +29,15 @@
         '(("t" "Todo" entry
            (file+headline "~/org/inbox.org" "Inbox")
            "* TODO %^{Task}\n:PROPERTIES:\n:CREATED: %U\n:CAPTURED: %a\n:END:\n%?")
-          ("e" "Event" entry
-           (file+headline "~/org/calendar.org" "Events")
+          ("a" "Appointment" entry
+           (file+headline "~/org/personal.org" "Appointments")
            "* %^{Event}\n%^{SCHEDULED}T\n:PROPERTIES:\n:CREATED: %U\n:CAPTURED: %a\n:END:\n%?")
-          ("d" "Deadline" entry
-           (file+headline "~/org/calendar.org" "Deadlines")
-           "* TODO %^{Task}\nDEADLINE: %^{Deadline}T\n:PROPERTIES:\n:CREATED: %U\n:CAPTURED: %a\n:END:\n%?")
           ("p" "Project" entry
            (file+headline "~/org/projects.org" "Projects")
            "* PROJ %^{Project name}\n:PROPERTIES:\n:CREATED: %U\n:CAPTURED: %a\n:END:\n** TODO %?")
+		  ("c" "Chore" entry
+           (file+headline "~/org/personal.org" "Chore")
+           "* %^{Event}\n%^{SCHEDULED}T\n:PROPERTIES:\n:CREATED: %U\n:CAPTURED: %a\n:END:\n%?")
           ("i" "Idea" entry
            (file+headline "~/org/ideas.org" "Ideas")
            "** IDEA %^{Idea}\n:PROPERTIES:\n:CREATED: %U\n:CAPTURED: %a\n:END:\n%?")
